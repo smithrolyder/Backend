367607cbf82173545f9a45d5426b824234f45cea
smithrolyder <smithrolyder@gmail.com>
2026-03-10 21:46:23 -0400

    update index.js

--- index.js
+++ index.js
@@ -5,24 +5,20 @@ const PORT = 3000;
 
 let livres = [
   {id: 1, tit: "Ti prens lan", ote: "Saint-Ecmxupery"},
-  {id: 2, tit: "Alchemist la", ote: "Paul Cohero" }
+  {id: 2, tit: "Alchemist la", ote: "Paul Cohero" },
+  { id: 3, tit: 'Kòd Da Vinci a', otè: 'Dan Brown' }
 ];
 
 app.get("/livres", (req, res)=>{
   res.send(livres);
 });
 
-app.post("/livres", (req, res)=>{
-  res.send("yon nouvo liv ajoute");
+app.get("/livres/:id", (req, res)=>{
+  const id = req.params.id;
+  console.log(`ID yo mande ${id}`);
+  
+  res.send(`Ou mande liv nimewo ${id}`);
 });
-
-app.put("/livres", (req, res)=>{
-  res.send("liv modifye");
-});
-
-app.delete("/livres", (req, res)=>{
-  res.send("liv efase");
-})
 
 app.listen(PORT, ()=>{
   console.log(`seve ap kouri sou http://localhost:${PORT}`);
