8d4606c249c64c86f861f865d61dd958cb8d918b
smithrolyder <smithrolyder@gmail.com>
2026-03-11 00:45:36 -0400

    update index.js

--- index.js
+++ index.js
@@ -4,10 +4,16 @@ const app = express();
 const PORT = 3000;
 
 let livres = [
-  {id: 1, tit: "Ti prens lan", ote: "Saint-Ecmxupery"},
+  {id: 1, tit: "Ti prens lan", ote: "Saint-Ecupery"},
   {id: 2, tit: "Alchemist la", ote: "Paul Cohero" },
   { id: 3, tit: 'Kòd Da Vinci a', otè: 'Dan Brown' }
 ];
+
+let auteurs = [
+  {id: 1, name: "Saint-Ecupery"},
+  {id: 2, name: "Paul Cohero"},
+  {id: 3, name: "Dan Brown"}
+]
 
 app.get("/livres", (req, res)=>{
   res.send(livres);
@@ -23,6 +29,21 @@ app.get("/livres/:id", (req, res)=>{
   }
   
   res.send(liv);
+});
+
+app.get("/auteurs", (req, res)=>{
+  res.send(auteurs);
+});
+app.get("/auteurs/:id", (req, res)=>{
+  const id = req.params.id;
+  
+  const auteur = auteurs.find(a => a.id === Number(id));
+  
+  if (!auteur) {
+    res.status(404).send("Dezole auteur sa pa disponib.");
+  }
+  
+  res.send(auteur);
 });
 
 app.listen(PORT, ()=>{
